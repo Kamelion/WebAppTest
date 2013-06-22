f53f91baf695efe7f33bf988199150a16e2cf4dd
Andy <alevak4291@gmail.com>
2013-06-22 14:45:01 -0400

    fifth commit

--- app/js/app.js
+++ app/js/app.js
@@ -5,6 +5,6 @@
 angular.module('myApp', ['myApp.filters', 'myApp.services', 'myApp.directives', 'myApp.controllers']).
   config(['$routeProvider', function($routeProvider) {
     $routeProvider.when('/view1', {templateUrl: '../partials/partial1.html', controller: 'MyCtrl1'});
-    $routeProvider.when('/signup', {templateUrl: '../partials/partial3.html', controller: 'MyCtrl1'});
+    $routeProvider.when('/signup', {templateUrl: '../partials/partial2.html', controller: 'MyCtrl1'});
     $routeProvider.otherwise({redirectTo: '/view1'});
   }]);
